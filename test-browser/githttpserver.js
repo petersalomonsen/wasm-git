8a1e9b74d1df01258059465af72b1021e2850b17
Peter Salomonsen <pjsalomonsen@gmail.com>
2021-01-01 06:46:24 +0100

    in memory git server

--- test-browser/githttpserver.js
+++ test-browser/githttpserver.js
@@ -2,6 +2,9 @@
  * This example will create a git http server to repositories on your local disk.
  * Set the GIT_PROJECT_ROOT environment variable to point to location of your git repositories.
  */
+const fs = require('fs');
+fs.writeFileSync('log.txt', '');
+console.log = (...msg) => fs.appendFileSync('log.txt', `${msg.join('\t')}\n`);
 
 function startServer() {
     const http = require('http');
@@ -14,7 +17,8 @@ function startServer() {
 
     const script = 'git';
 
-    const gitcgi = cgi(script, {args: ['http-backend'],
+    const gitcgi = cgi(script, {
+        args: ['http-backend'],
         stderr: process.stderr,
         env: {
             'GIT_PROJECT_ROOT': tmpdir(),
@@ -23,17 +27,19 @@ function startServer() {
         }
     });
 
-    return http.createServer( (request, response) => {
+    return http.createServer((request, response) => {
         let path = request.url.substring(1);
 
-        console.log('git http server request', request.url);
-        
+        console.log('git http server request', request.url, request.method);
+
         if (path.indexOf('ping') > -1) {
             response.statusCode = 200;
             response.end('pong');
-        } else if( path.indexOf('git-upload') > -1 ||
-            path.indexOf('git-receive') > -1) {
-            gitcgi(request, response);
+        } else if (path.indexOf('git-upload') > -1 && request.method === 'GET') {
+            response.end("001e# service=git-upload-pack\n00000000\n");
+        } else if (path.indexOf('git-receive') > -1 && request.method === 'GET') {
+
+            response.end('009f0000000000000000000000000000000000000000\n0000');
         } else {
             response.statusCode = 404;
             response.end('not found');
--- test-browser/test.spec.js
+++ test-browser/test.spec.js
@@ -1,4 +1,4 @@
-describe('wasm-git', function() {
+describe('wasm-git', function () {
     const worker = new Worker('base/worker.js');
 
     const workerReadyPromise = new Promise(resolve => {
@@ -20,28 +20,28 @@ describe('wasm-git', function() {
     });
 
     it('should sync local idbfs and find no repository', async () => {
-        worker.postMessage({command: 'synclocal', url: `${location.origin}/testrepo.git`});
+        worker.postMessage({ command: 'synclocal', url: `${location.origin}/testrepo.git` });
         let result = await new Promise(resolve =>
             worker.onmessage = msg => {
                 if (msg.data.notfound) {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert(result.data.notfound);
     });
 
     it('should clone a bare repository and push commits', async () => {
-        worker.postMessage({command: 'clone', url: `${location.origin}/testrepo.git`});
+        worker.postMessage({ command: 'clone', url: `${location.origin}/testrepo.git` });
         let result = await new Promise(resolve =>
             worker.onmessage = msg => {
                 if (msg.data.dircontents) {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert(result.data.dircontents.length > 2);
@@ -58,64 +58,64 @@ describe('wasm-git', function() {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert(result.data.dircontents.find(entry => entry === 'test.txt'));
     });
 
     it('remove the local clone of the repository', async () => {
-        worker.postMessage({command: 'deletelocal'});
+        worker.postMessage({ command: 'deletelocal' });
         let result = await new Promise(resolve =>
             worker.onmessage = msg => {
                 if (msg.data.deleted) {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert.equal(result.data.deleted, 'testrepo.git');
     });
 
     it('should clone the repository with contents', async () => {
-        worker.postMessage({command: 'readfile', filename: 'test.txt'});
+        worker.postMessage({ command: 'readfile', filename: 'test.txt' });
         let result = await new Promise(resolve =>
             worker.onmessage = msg => {
                 if (msg.data.stderr) {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert.exists(result.data.stderr);
 
-        worker.postMessage({command: 'clone', url: `${location.origin}/testrepo.git`});
+        worker.postMessage({ command: 'clone', url: `${location.origin}/testrepo.git` });
         result = await new Promise(resolve =>
             worker.onmessage = msg => {
                 if (msg.data.dircontents) {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert(result.data.dircontents.length > 2);
         assert(result.data.dircontents.find(entry => entry === '.git'));
         assert(result.data.dircontents.find(entry => entry === 'test.txt'));
 
-        worker.postMessage({command: 'readfile', filename: 'test.txt'});
+        worker.postMessage({ command: 'readfile', filename: 'test.txt' });
         result = await new Promise(resolve =>
             worker.onmessage = msg => {
                 if (msg.data.filecontents) {
                     resolve(msg);
                 } else {
                     console.log(msg.data);
-                }                
+                }
             }
         );
         assert.equal(result.data.filecontents, 'hello world!');
-        
+
     });
 });
