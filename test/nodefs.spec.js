9f5c38591636547aa1d3939459195c7bf242b702
Peter Salomonsen <pjsalomonsen@gmail.com>
2021-04-07 17:37:37 +0200

    github actions test and release pipeline
    add nodefs clone to test specs
    v0.0.5
    
    fixes #23

---
+++ test/nodefs.spec.js
@@ -0,0 +1,35 @@
+const lgPromise = require('./common.js').lgPromise;
+const assert = require('assert');
+const { rmSync } = require('fs');
+
+describe.only('nodefs', function () {
+    this.timeout(20000);
+
+    it('should clone using nodefs', async () => {        
+        const lg = await lgPromise;
+
+        const FS = lg.FS;
+        const NODEFS = FS.filesystems.NODEFS;
+        const clonedir = 'nodefsclonetest';
+
+        FS.mkdir('/nodefs');
+        FS.mount(NODEFS, { root: '.' }, '/nodefs');
+        FS.chdir('/nodefs');
+
+        FS.writeFile('/home/web_user/.gitconfig', '[user]\n' +
+            'name = Test User\n' +
+            'email = test@example.com');
+
+        // clone a repository from github
+        lg.callMain(['clone', 'https://github.com/petersalomonsen/wasm-git.git', clonedir]);
+
+        FS.chdir(clonedir);
+        console.log(FS.readdir('.'));
+        lg.callMain(['log']);
+
+        assert(FS.readdir('.').indexOf('README.md') > -1);
+        FS.chdir('..');
+        rmSync('clonedir', {recursive: true, force: true});
+        console.log('clone to nodefs suceeded');
+    });
+});
