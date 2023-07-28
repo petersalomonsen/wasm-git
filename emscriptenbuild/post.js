fc9fbef77a91d7dbe24342dcc3ee9a855ab1aa7f
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-07-28 20:24:50 +0200

    fix: no need for chmod patch (#82)
    resolves #81

--- emscriptenbuild/post.js
+++ emscriptenbuild/post.js
@@ -12,18 +12,6 @@
 const emscriptenhttpconnections = {};
 let httpConnectionNo = 0;
 
-const chmod = FS.chmod;
-    
-FS.chmod = function(path, mode, dontFollow) { 
-    if (mode === 0o100000 > 0) {
-        // workaround for libgit2 calling chmod with only S_IFREG set (permisions 0000)
-        // reason currently not known
-        return chmod(path, mode, dontFollow);
-    } else {
-        return 0;
-    }
-};
-
 if(ENVIRONMENT_IS_WORKER) {
     Object.assign(Module, {
         emscriptenhttpconnect: function(url, buffersize, method, headers) {
