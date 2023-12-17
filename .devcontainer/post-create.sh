a00035c90a44795cb1876ece9c881e990993aab3
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-12-17 13:33:33 +0000

    wasi build

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+curl https://wasmtime.dev/install.sh -sSf | bash
 npm install
 sh setup.sh
 
@@ -9,3 +9,21 @@ git pull
 ./emsdk install latest
 ./emsdk activate latest
 cd ..
+
+sudo apt update
+sudo apt install -y clang-12
+sudo apt install -y lld-12
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-12 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-12 100
+
+sudo ln -s /usr/bin/wasm-ld-12 /usr/bin/wasm-ld
+
+cd wasibuild
+
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-20/wasi-sdk-20.0-linux.tar.gz
+tar -xvzf wasi-sdk-20.0-linux.tar.gz
+sudo cp -r wasi-sdk-20.0/lib/clang/16/lib/wasi /usr/lib/llvm-12/lib/clang/12.0.0/lib/
+
+cd ..
+
+curl https://wasmtime.dev/install.sh -sSf | bash
