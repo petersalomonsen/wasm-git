4d69fd94d43baced2b599da7c96498b243e49517
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-12-17 18:01:23 +0000

    setup ar and ranlib in toolchain

--- setup_wasi.sh
+++ setup_wasi.sh
@@ -4,3 +4,7 @@ curl -L https://github.com/libgit2/libgit2/archive/refs/tags/v1.7.1.tar.gz --out
 tar -xzf libgit2.tar.gz
 mv libgit2-1.7.1 libgit2
 rm libgit2.tar.gz
+rm libgit2/src/libgit2/transports/http.c
+rm libgit2/src/libgit2/streams/socket.c
+rm libgit2/src/libgit2/sysdir.c
+cp -r libgit2patchedfiles/examples/* libgit2/examples/
