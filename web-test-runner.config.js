00dc5986a113ca9da9a6da3d943101c4c2079e9f
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-05-22 18:19:39 +0000

    remote.spec.js works

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,4 +1,7 @@
 import { playwrightLauncher } from '@web/test-runner-playwright';
+import { startServer } from './test-browser/githttpserver.js';
+
+startServer();
 
 export default {
   files: [
@@ -25,7 +28,17 @@ export default {
       </body>
     </html>`,
   browsers: [
-    playwrightLauncher({ product: 'chromium', launchOptions: { args: ['--headless'] } }),
+    playwrightLauncher({ product: 'chromium', createBrowserContext: async ({ browser }) => {
+
+      const ctx = await browser.newContext({});
+      await ctx.route(/http:\/\/localhost:8000\/.*\.git\/.*/, async (route) => {
+        const url = route.request().url();
+        const response = await route.fetch({url: url.replace(':8000/', ':8080/')});
+        const body = await response.body();
+        await route.fulfill({ body });
+      });
+      return ctx;
+    }, }),
     /*playwrightLauncher({
       product: 'firefox', launchOptions: {
         headless: false,
