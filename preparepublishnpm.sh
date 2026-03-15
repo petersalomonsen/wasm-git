77ba8fff21bedb598566acf86f588b0aa449ed33
Peter Salomonsen <pjsalomonsen@gmail.com>
2026-03-15 06:47:30 +0000

    Prepare v0.0.14 release, include OPFS files in publish script

--- preparepublishnpm.sh
+++ preparepublishnpm.sh
@@ -5,5 +5,7 @@ cp emscriptenbuild/libgit2/examples/lg2.wasm .
 cp emscriptenbuild/libgit2/examples/lg2.js .
 cp emscriptenbuild/libgit2/examples/lg2_async.wasm .
 cp emscriptenbuild/libgit2/examples/lg2_async.js .
+cp emscriptenbuild/libgit2/examples/lg2_opfs.wasm .
+cp emscriptenbuild/libgit2/examples/lg2_opfs.js .
 echo "publish --dry-run (run npm publish to finalize)"
 npm publish --dry-run
